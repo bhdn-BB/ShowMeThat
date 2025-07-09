7d9d28a8707b6773d8c7be17d8fa90c05e3d226a
Bohdan Boklah <b.boklag@gmail.com>
2025-07-09 11:45:29 +0300

    Initial commit

---
+++ UI_service/streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+if __name__ == '__main__':
+
+    st.set_page_config(
+        page_title="ShowMeThat 🔍",
+        page_icon="🎥",
+        layout="centered",
+        initial_sidebar_state="auto",
+    )
